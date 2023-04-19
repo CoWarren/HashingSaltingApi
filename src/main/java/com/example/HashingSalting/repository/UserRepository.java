11b0c3bccaaa46224473942c7264f269eb5de504
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-04-19 16:47:14 +0100

    adding imports to user repository, and adding findByUsername method

--- src/main/java/com/example/HashingSalting/repository/UserRepository.java
+++ src/main/java/com/example/HashingSalting/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.HashingSalting.repository;
 
-public interface UserRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.HashingSalting.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    List<User> findByUsername(String username);
 }
