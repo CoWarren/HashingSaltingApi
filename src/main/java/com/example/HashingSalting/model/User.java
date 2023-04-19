5d5087d8bbbbc91b3b2190c477c10614ee3f3800
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-04-19 16:06:47 +0100

    created the user model

--- src/main/java/com/example/HashingSalting/model/User.java
+++ src/main/java/com/example/HashingSalting/model/User.java
@@ -1,3 +1,43 @@
 package com.example.HashingSalting.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name="user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    // declaring the variable id to being private and of 'long' datatype
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
