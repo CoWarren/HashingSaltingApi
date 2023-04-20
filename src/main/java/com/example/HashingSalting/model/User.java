2c81803ef38212da505d028acdfaf913143e3b4d
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-04-20 10:59:55 +0100

    created a constructor for being able to make a new user

--- src/main/java/com/example/HashingSalting/model/User.java
+++ src/main/java/com/example/HashingSalting/model/User.java
@@ -19,6 +19,12 @@ public class User {
     private String username;
     private String password;
 
+    public User (long id, String username, String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
     public long getId() {
         return id;
     }
