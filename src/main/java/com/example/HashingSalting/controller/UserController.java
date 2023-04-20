9b09eeae5b65750b37f19dbb3a8794fe7eae9ed8
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-04-20 15:48:29 +0100

    created a getter for getting all users

--- src/main/java/com/example/HashingSalting/controller/UserController.java
+++ src/main/java/com/example/HashingSalting/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.HashingSalting.model.User;
 
 import com.example.HashingSalting.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
