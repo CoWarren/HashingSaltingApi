944ca657e9f425ece9a90e9c7e247dafc2a28f45
Chris-warren <chris-warrentodd@hollandandbarrett.com>
2023-04-20 10:52:50 +0100

    created a get users mapping that finds all the users

--- src/main/java/com/example/HashingSalting/controller/UserController.java
+++ src/main/java/com/example/HashingSalting/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.example.HashingSalting.controller;
 
+import com.example.HashingSalting.model.User;
+
+import com.example.HashingSalting.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/users")
+    public @ResponseBody List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
 }
